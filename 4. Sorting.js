663e0785b8849623c0a642de71a21f128b7f38dc
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-17 23:36:51 +0300

    Add files via upload

---
+++ 4. Sorting.js
@@ -0,0 +1,16 @@
+function sorting(arr) {
+    let newArr = arr.sort((a, b) => a - b);
+    let finalArr = [];
+    
+    while (newArr.length > 0) {
+        let endNum = newArr.pop();
+        finalArr.push(endNum);
+        if (newArr.length > 0) {
+            let firstNum = newArr.shift();
+            finalArr.push(firstNum);
+        }
+    }
+    console.log(finalArr.join(` `));
+}
+sorting([1, 21, 3, 52, 69, 63, 31, 2, 18, 94]);
+//sorting([34, 2, 32, 45, 690, 6, 32, 7, 19, 47]);
